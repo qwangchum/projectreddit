f002197e93034172d4f0a20f7b841f5c3888b804
qwangchum <lololotto666@gmail.com>
2024-05-21 08:04:47 +0000

    ProjectData_Engineering_reddit

---
+++ dags/reddit_dag.py
@@ -0,0 +1,143 @@
+#constants
+import configparser
+import os
+from airflow.models import Variable
+
+client_id = Variable.get("client_id")
+client_secret = Variable.get("client_secret")
+user_agent = 'qwang_project2'
+username = 'qwang_kk'
+
+INPUT_PATH = './data/input/'
+OUTPUT_PATH = './data/output'
+
+
+POST_FIELDS = (
+    'id',
+    'title',
+    'selftext',
+    'score',
+    'num_comments',
+    'author',
+    'created_utc',
+    'url',
+    'upvote_ratio',
+    'over_18',
+    'edited',
+    'spoiler',
+    'stickied'
+)
+
+
+#reddit_etl
+import sys
+
+import numpy as np
+import pandas as pd
+import praw
+from praw import Reddit
+
+
+def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
+    try:
+        reddit = praw.Reddit(client_id=client_id,
+                             client_secret=client_secret,
+                             user_agent=user_agent)
+        print("connected to reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
+    subreddit = reddit_instance.subreddit(subreddit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
+
+    post_lists = []
+
+    for post in posts:
+        post_dict = vars(post)
+        post = {key: post_dict[key] for key in POST_FIELDS}
+        post_lists.append(post)
+
+    return post_lists
+
+
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
+    edited_mode = post_df['edited'].mode()
+    post_df['edited'] = np.where(post_df['edited'].isin([True, False]),
+                                 post_df['edited'], edited_mode).astype(bool)
+    post_df['num_comments'] = post_df['num_comments'].astype(int)
+    post_df['score'] = post_df['score'].astype(int)
+    post_df['title'] = post_df['title'].astype(str)
+
+    return post_df
+
+
+def load_data_to_csv(data: pd.DataFrame, path: str):
+    data.to_csv(path, index=False)
+
+
+
+#reddit_pipeline
+
+import pandas as pd
+
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
+    # connecting to reddit instance
+    instance = connect_reddit(client_id, client_secret, 'qwang_project2')
+    # extraction
+    posts = extract_posts(instance, subreddit, time_filter, limit)
+    post_df = pd.DataFrame(posts)
+    # transformation
+    post_df = transform_data(post_df)
+    # loading to csv
+    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+    load_data_to_csv(post_df, file_path)
+
+    return file_path
+
+#DAG
+import sys
+import os
+from airflow.utils import timezone
+from datetime import datetime
+
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__name__))))
+
+default_args = {
+    'owner': 'qwangchum',
+    'start_date': datetime(2024, 5, 1)
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id='etl_reddit_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
+)
+
+# extraction from reddit
+extract = PythonOperator(
+    task_id='reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'dataengineering',
+        'time_filter': 'day',
+        'limit': 100
+    },
+    dag=dag
+)
+
